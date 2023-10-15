a312e561d7143fc157ff7a85f26ee7ebe7d10b7c
norchard <norchard24@gmail.com>
2023-10-15 15:25:07 -0500

    Remove distDir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: "dist",
   images: {
     domains: ["images.unsplash.com", "cdn.sanity.io"],
   },
