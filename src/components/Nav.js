67a4073f66c89c9c4312ea6d561137a878cb2f62
norchard <norchard24@gmail.com>
2023-11-04 11:32:51 -0500

    Update mobile menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,35 @@ export default function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="p-11 pb-20 relative">
+    <header className=" pb-20 relative">
+      {menuOpen && (
+        <div className="w-full text-center bg-white menu md:flex float-right">
+          <Link
+            onClick={() => setMenuOpen(false)}
+            className="text-xl mt-5 mb-5 mr-6 text-gray-500 hover:text-gray-800"
+            href="/about"
+          >
+            About
+          </Link>
+          <Link
+            onClick={() => setMenuOpen(false)}
+            className="text-xl mt-5 mb-5 mr-6 text-gray-500 hover:text-gray-800"
+            href="/artists"
+          >
+            Artists
+          </Link>
+          <Link
+            onClick={() => setMenuOpen(false)}
+            className="text-xl mt-5 mb-5 mr-6 text-gray-500 hover:text-gray-800"
+            href="/contact"
+          >
+            Contact
+          </Link>
+        </div>
+      )}
       <Link href="/">
         <Image
-          className="float-left"
+          className="m-11 float-left"
           src="/Zoya_logo.png"
           alt="zoya logo"
           width={200}
@@ -39,7 +64,7 @@ export default function Nav() {
       </div>
       <div className="xs:flex float-right md:hidden">
         <button
-          class="navbar-burger flex items-center"
+          class="m-14 navbar-burger flex items-center"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           <svg
@@ -52,31 +77,6 @@ export default function Nav() {
           </svg>
         </button>
       </div>
-      {menuOpen && (
-        <div className="w-full text-center bg-white menu md:flex float-right">
-          <Link
-            onClick={() => setMenuOpen(false)}
-            className="text-xl mt-5 mb-5 mr-6 text-gray-500 hover:text-gray-800"
-            href="/about"
-          >
-            About
-          </Link>
-          <Link
-            onClick={() => setMenuOpen(false)}
-            className="text-xl mt-5 mb-5 mr-6 text-gray-500 hover:text-gray-800"
-            href="/artists"
-          >
-            Artists
-          </Link>
-          <Link
-            onClick={() => setMenuOpen(false)}
-            className="text-xl mt-5 mb-5 mr-6 text-gray-500 hover:text-gray-800"
-            href="/contact"
-          >
-            Contact
-          </Link>
-        </div>
-      )}
     </header>
   );
 }
